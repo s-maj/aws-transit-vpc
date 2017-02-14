27253f1044bd275a7f55efe20d2b7cac2f4050ee
majs <majs@dnb.com>
2017-02-14 10:39:04 +0000

    vpn config generator initial commit

---
+++ ansible/roles/generate_vpn_config/files/vpn_config_generator.py
@@ -0,0 +1,89 @@
+import json
+import urllib2
+from collections import OrderedDict
+
+from boto3 import Session
+from xmldict import xml_to_dict
+
+
+def get_regions():
+    regions_list = []
+    session = Session(region_name='us-east-1')
+    ec2 = session.client('ec2')
+    regions = ec2.describe_regions()
+    for region in regions['Regions']:
+        regions_list.append(region['RegionName'])
+
+    return regions_list
+
+
+def get_vpn_config(regions, public_ip):
+    global_config = OrderedDict()
+    for region in regions:
+        print("Looking in %s" % region)
+        session = Session(region_name=region)
+        ec2 = session.client('ec2')
+
+        response = ec2.describe_vpn_connections(
+            Filters=[
+                {
+                    'Name': 'tag:BIRD',
+                    'Values': [
+                        'True',
+                    ]
+                },
+                {
+                    'Name': 'tag:ID',
+                    'Values': [
+                        public_ip,
+                    ]
+                },
+                {
+                    'Name': 'state',
+                    'Values': [
+                        'available'
+                    ]
+                }
+            ]
+        )
+        local_config = parse_vpn_response(response)
+        global_config.update(local_config)
+
+    return global_config
+
+
+def parse_vpn_response(response):
+    config = OrderedDict()
+    for connection in response['VpnConnections']:
+        vpn_id = connection['VpnConnectionId']
+        config[vpn_id] = OrderedDict()
+        vpn_config = xml_to_dict(connection['CustomerGatewayConfiguration'])
+
+        for index, tunnel in enumerate(vpn_config['vpn_connection']['ipsec_tunnel']):
+            tunnel_key = 'tunnel-%s' % index
+            config[vpn_id][tunnel_key] = OrderedDict()
+
+            config[vpn_id][tunnel_key]['right_inside_ip'] = '%s/%s' % (
+                tunnel['customer_gateway']['tunnel_inside_address']['ip_address'],
+                tunnel['customer_gateway']['tunnel_inside_address']['network_cidr'])
+            config[vpn_id][tunnel_key]['right_outside_ip'] = tunnel['customer_gateway']['tunnel_outside_address'][
+                'ip_address']
+            config[vpn_id][tunnel_key]['right_asn'] = tunnel['customer_gateway']['bgp']['asn']
+            config[vpn_id][tunnel_key]['left_inside_ip'] = '%s/%s' % (
+                tunnel['vpn_gateway']['tunnel_inside_address']['ip_address'],
+                tunnel['vpn_gateway']['tunnel_inside_address']['network_cidr'])
+            config[vpn_id][tunnel_key]['left_outside_ip'] = tunnel['vpn_gateway']['tunnel_outside_address'][
+                'ip_address']
+            config[vpn_id][tunnel_key]['left_asn'] = tunnel['vpn_gateway']['bgp']['asn']
+            config[vpn_id][tunnel_key]['psk'] = tunnel['ike']['pre_shared_key']
+
+    return config
+
+
+public_ip = urllib2.urlopen("http://169.254.169.254/latest/meta-data/public-ipv4").read()
+all_regions = get_regions()
+config = get_vpn_config(all_regions, public_ip)
+
+print("Writing config file")
+with open('/tmp/vpn_conf.json', 'w') as f:
+    f.write(json.dumps(config))
