304b82cde1fe44c41f55c8d740d809510ab34b72
majs <majs@dnb.com>
2017-02-15 12:32:32 +0000

    more or less final configuration for test environment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,51 @@
 module "satellite-nvirginia" {
   source = "./satellite_module"
 
-  name          = "satellite-nvirginia"
-  cidr_block    = "192.168.220.0/24"
-  public_a_cidr = "192.168.220.0/25"
-  public_b_cidr = "192.168.220.128/25"
-  cgw_asn_list   = [ "65000", "65001" ]
-  cgw_ip_list   = "${module.vpn-server.elastic_ips}"
-  region        = "us-east-1"
+  name             = "satellite-nvirginia"
+  cidr_block       = "192.168.220.0/24"
+  public_cidr_list = [ "192.168.220.0/25", "192.168.220.128/25" ]
+  cgw_asn_list     = [ "65000", "65001" ]
+  cgw_ip_list      = "${module.vpn-server.elastic_ips}"
+  region           = "us-east-1"
 }
 
 module "satellite-oregon" {
   source = "./satellite_module"
 
-  name          = "satellite-nvirginia"
-  cidr_block    = "192.168.221.0/24"
-  public_a_cidr = "192.168.221.0/25"
-  public_b_cidr = "192.168.221.128/25"
-  cgw_asn_list  = [ "65000", "65001" ]
-  cgw_ip_list   = "${module.vpn-server.elastic_ips}"
+  name             = "satellite-nvirginia"
+  cidr_block       = "192.168.221.0/24"
+  public_cidr_list = [ "192.168.221.0/25", "192.168.221.128/25" ]
+  cgw_asn_list     = [ "65000", "65001" ]
+  cgw_ip_list      = "${module.vpn-server.elastic_ips}"
+  region           = "us-west-2"
+}
+
+module "vpn-probe-satellite-nvirginia" {
+  source = "./probe_module"
+
+  vpc_id        = "${module.satellite-nvirginia.vpc_id}"
+  subnet_id     = "${module.satellite-nvirginia.subnet_id[0]}"
+  instance_type = "t2.nano"
+  key_name      = "AWSlab"
+  region        = "us-east-1"
+
+  tags =  {
+    Name       = "satellite-nvirginia-probe",
+  }
+}
+
+module "vpn-probe-satellite-oregon" {
+  source = "./probe_module"
+
+  vpc_id        = "${module.satellite-oregon.vpc_id}"
+  subnet_id     = "${module.satellite-oregon.subnet_id[0]}"
+  instance_type = "t2.nano"
+  key_name      = "AWSlab"
   region        = "us-west-2"
+
+  tags =  {
+    Name       = "satellite-nvirginia-probe",
+  }
 }
 
 module "vpn-server" {
@@ -30,11 +56,12 @@ module "vpn-server" {
   instance_type  = "t2.small"
   key_name       = "AWSlab"
   instance_count = 2
+  asn_list       = [ "65000", "65001" ]
   region         = "eu-west-1"
 
   tags =  {
     Name       = "strongSwan",
-    BIRD       = "True",
+    bird       = "True",
     strongSwan = "True"
   }
 }
